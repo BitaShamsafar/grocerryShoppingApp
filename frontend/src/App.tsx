cd0c98da395f1810afa5953e523a0822a2f97fcc
Eya <benhasseneya28@gmail.com>
2026-01-28 14:25:02 +0100

    Add frontend feature 1 (show lists)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
+import ShoppingLists from "./components/ShoppingLists.tsx";
 
 function App() {
 
-
   return (
     <>
+        <nav>
+            <Link to="/">Home</Link> | <Link to="/lists"> My Shopping Lists </Link>
+        </nav>
+
      <Routes>
          <Route path="/" element={<h1>Grocery Shopping App</h1>} />
+         <Route path="/lists" element={<ShoppingLists />} />
      </Routes>
     </>
   )
